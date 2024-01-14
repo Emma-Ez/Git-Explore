0343bd1ef75d92181cb55bcab4758f0be88eabd7
Emma-Ez <emma.favoured@yahoo.com>
2024-01-14 20:36:03 +0100

    Removed emoji and corrected typo

--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
-tradeProfit = int(input())
+tradeProfit = int(input("Profit: "))
 if tradeProfit >= 1000:
     print("A new day has come")
 elif tradeProfit >= 500:
     print("Agba Baller")
 elif tradeProfit < 0:
-    print("Everwhere id just bloody")
+    print("Everwhere is just bloody")
 else:
     print("Half bread is better than Akara!")
