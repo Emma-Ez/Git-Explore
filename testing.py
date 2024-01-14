8204756e908137bcbf35f171a1c1c4eaf9777b62
Emma-Ez <emma.favoured@yahoo.com>
2024-01-14 20:14:16 +0100

    My first commit

---
+++ testing.py
@@ -0,0 +1,9 @@
+tradeProfit = int(input())
+if tradeProfit >= 1000:
+    print("A new day has come")
+elif tradeProfit >= 500:
+    print("Agba Baller")
+elif tradeProfit < 0:
+    print("Everwhere id just bloody")
+else:
+    print("Half bread is better than Akara!")
